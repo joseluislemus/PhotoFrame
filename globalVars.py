f7b203e6520487e73b02461b6c5c51db7949560e
joseluislemus <jllemus@gmail.com>
2022-06-18 10:52:02 +0200

    changed exif data

--- globalVars.py
+++ globalVars.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
+#%% -*- coding: utf-8 -*-
 
 import glob 
 from datetime import datetime
 import pandas as pd
 from PIL import Image,ExifTags
-
+from fractions import Fraction
 
 #global vars
 delay_seconds = 30
@@ -28,27 +28,26 @@ nav_previous = False
 
 
 image_df = pd.DataFrame(columns=['date','filename','speed','aperture','iso','focallength','camera'])
-for image_file in image_files:
-    image_dict = {}
-    image_dict['filename'] = image_file
+for index,image_file in enumerate(image_files):
+    
+    image_df.loc[index,'filename'] = image_file
     image = Image.open(image_file)
     exif_data = {
                 ExifTags.TAGS[k]: v
                 for k, v in image._getexif().items()
                 if k in ExifTags.TAGS
                 }
-    image_dict['date'] = exif_data['DateTimeDigitized']
-    image_dict['speed'] = str(exif_data['ExposureTime'][0]) + '/' + str(exif_data['ExposureTime'][1])+'s'
-    image_dict['aperture'] = 'F'+str(exif_data['FNumber'][0] / exif_data['FNumber'][1])
-    image_dict['focallength'] = str(int(exif_data['FocalLength'][0] / exif_data['FocalLength'][1]))+'mm'
-    image_dict['iso'] = str(exif_data['ISOSpeedRatings'])
+    image_df.loc[index,'date'] = exif_data['DateTimeDigitized']
+    image_df.loc[index,'speed'] = str(Fraction(exif_data['ExposureTime'])) + 's'
+    image_df.loc[index,'aperture'] = 'F'+str(exif_data['FNumber'])
+    image_df.loc[index,'focallength'] = str(int(exif_data['FocalLength']))+'mm'
+    image_df.loc[index,'iso'] = str(exif_data['ISOSpeedRatings'])
     
     if exif_data['Make'] in exif_data['Model']:
-        image_dict['camera'] = (exif_data['Model']).upper()
+        image_df.loc[index,'camera'] = (exif_data['Model']).upper()
     else:
-        image_dict['camera'] = (exif_data['Make'] + ' ' + exif_data['Model']).upper()
+        image_df.loc[index,'camera'] = (exif_data['Make'] + ' ' + exif_data['Model']).upper()
         
-    image_df=image_df.append(image_dict,ignore_index=True)
 
 image_df['date']=pd.to_datetime(image_df['date'], format='%Y:%m:%d %H:%M:%S')
 image_df=image_df.sort_values(by="date")
